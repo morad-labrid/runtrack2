8c348b8bb4f2fcb88ee78cc448a57db4d808d4a4
Morad <morad.labrid@laplateforme.io>
2020-11-02 11:58:32 +0100

    la creation de mon premier PHP (Hello LaPlateforme)

---
+++ Jour01/Job00/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <main>
+    <?php
+    $message = "Hello LaPlateforme";
+    echo $message;
+
+    ?>
+    
+    
+    </main>
+    <footer></footer>
+</body>
+</html>
