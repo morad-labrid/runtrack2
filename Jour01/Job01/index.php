8d018e47ccd0b4bd372a54e2069d1fe1579cf6ad
Morad <morad.labrid@laplateforme.io>
2020-11-02 14:14:49 +0100

    Commit du deuxième job01

---
+++ Jour01/Job01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+    $str = "LaPlateforme";
+    echo $str;
+    $str2 = "Vive";
+    $str3 = "!";
+    echo $str2. ' '. $str. ' '. $str3. ' ';
+    $val = 6;
+    echo $val. ' ';
+    echo $val + 4;
+    $myBool = false;
+    echo $myBool;
+    
+?>
