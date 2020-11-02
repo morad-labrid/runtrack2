f68acaab97cdb0cddf09b67530adf46b1f164e39
Morad <morad.labrid@laplateforme.io>
2020-11-02 14:20:49 +0100

    modification du job01

--- Jour01/Job01/index.php
+++ Jour01/Job01/index.php
@@ -2,13 +2,19 @@
 
     $str = "LaPlateforme";
     echo $str;
+    echo "<br>";
     $str2 = "Vive";
     $str3 = "!";
     echo $str2. ' '. $str. ' '. $str3. ' ';
+    echo "<br>";
     $val = 6;
     echo $val. ' ';
+    echo "<br>";
     echo $val + 4;
+    echo "<br>";
+    $myBool = true;
+    echo $myBool;
     $myBool = false;
     echo $myBool;
-    
+
 ?>
