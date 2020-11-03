a6c85a73146fc40454c061ff501947c11fefc92d
Morad <morad.labrid@laplateforme.io>
2020-11-03 13:26:44 +0100

    Troisieme job

---
+++ Jour02/Job03/index.php
@@ -0,0 +1,26 @@
+<?php
+
+    $i = 0;
+    $logo = "La Plateforme_";
+
+    while( $i <= 100 ) {
+
+            switch ($i) {
+                case '42': //si le numero est egale 42  affiche La Plateforme_
+                    echo "La Plateforme_<br>";
+                    break;
+                
+                default: // affichage par defaut
+                    if($i <= 20) // si inferieur a 20 
+                        echo "<i>$i</i><br>"; // affiche les chiffre en italic
+                    elseif ($i >= 25 && $i <= 50) { // sinon si entre 25 et 50 efficher en gras
+                        echo "<u>$i</u><br>";
+                    }
+                    else // sinon affichage normal
+                        echo "$i<br>";
+                        break;
+            }
+            $i++;
+        }
+
+?>
