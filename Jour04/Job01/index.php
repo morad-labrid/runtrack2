940ba6b34951d84c2f1ae2b99fe63633799ee03a
Morad <morad.labrid@laplateforme.io>
2020-11-05 10:24:23 +0100

    jour 4

--- Jour04/Job01/index.php
+++ Jour04/Job01/index.php
@@ -11,6 +11,10 @@
     <label for="E-mail">Votre E-mail</label>
     <input type="email" name="email" id="email">
     <br>
+    Je suis
+    <input type="radio" name="Genre" value="Femme"> une femme
+    <input type="radio" name="Genre" value="Homme"> un homme
+    <br>
     <input type="submit" value="Envoyer">
 
 </form>
